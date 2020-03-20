fbdd7bc504a3d11c96fd94fe3231abb85bb7df36
marcelo.barrera <marcelo.barrera@abax.no>
2020-03-20 14:47:11 +0100

    listening to events geeting info from event object

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,33 +14,15 @@
     <div>
         <button v-on:click="increase">click me </button>
         <p>{{counter}}</p>
+        <p v-on:mousemove="updateCoordinates">
+          Coordinates: {{x}} / {{y}}
+        </p>
     </div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </p>    
   </div>
 </template>
 
@@ -53,6 +35,8 @@ export default {
       age: 55,
       someImageUrl:"http://placekitten.com/200/300",
       counter: 0,
+      x:0,
+      y:0,
     };
   },
   methods: {
@@ -62,6 +46,10 @@ export default {
     increase() {
       this.counter++
     },
+    updateCoordinates: function (event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    }
   },
   props: {
     msg: String
